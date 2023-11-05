f01fc7c4048d8958ede914e2c88fa205ef506920
kryb4k <karolina.rybak@smartbear.com>
2023-11-05 12:07:06 +0100

    project initialization

---
+++ server/src/main/java/todo/list/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package todo.list.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+
+}
