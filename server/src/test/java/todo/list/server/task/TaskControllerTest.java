f34ea1d2c66aa3eff9b90505fddd63241b3529c5
kryb4k <karolina.rybak@smartbear.com>
2023-11-24 12:02:36 +0100

    docker compose

--- server/src/test/java/todo/list/server/task/TaskControllerTest.java
+++ server/src/test/java/todo/list/server/task/TaskControllerTest.java
@@ -5,8 +5,4 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TaskControllerTest {
-    @Test
-    void test1() {
-        assertEquals(1,0);
-    }
 }
